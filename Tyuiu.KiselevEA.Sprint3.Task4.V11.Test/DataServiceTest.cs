f020862c04574bcdeeffed1b683440587c4b8f36
Tyuiu.KiselevEA@outlook.com <Tyuiu.KiselevEA@outlook.com>
2024-10-27 17:03:43 +0500

    T4

---
+++ Tyuiu.KiselevEA.Sprint3.Task4.V11.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+using Tyuiu.KiselevEA.Sprint3.Task4.V11.Lib;
+namespace Tyuiu.KiselevEA.Sprint3.Task4.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+
+            double wait = 48.294;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KiselevEA.Sprint3.Task4.V11/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.KiselevEA.Sprint3.Task4.V11.Lib;
+namespace Tyuiu.KiselevEA.Sprint3.Task4.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Киселев Е. А. | СМАРТб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Киселев Егор Алексеевич | СМАРТб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя операторы contunie и brake.                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+
+            Console.WriteLine("Старт" + startValue);
+            Console.WriteLine("Стоп" + stopValue);
+            
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Итоговая строка = " + ds.Calculate(startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
